c4e4ed4374c6541c986804478c04d86a335829ed
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-11 17:01:29 +0530

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+function Navbar(props) {
+	return (
+		<nav className='w-full flex justify-between p-4 bg-gray-800'>
+			<h1 className='font-bold uppercase text-xl'>
+				<a className='text-indigo-400' href='/'>
+					App Name
+				</a>
+			</h1>
+			<div className='status'>
+				<span className='text-yellow-400 mr-3'>Connection:</span>
+				{props.connectionStatus ? (
+					<span className='text-green-400 uppercase'>Good</span>
+				) : (
+					<span className='text-red-400 uppercase'>Disconnected</span>
+				)}
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
